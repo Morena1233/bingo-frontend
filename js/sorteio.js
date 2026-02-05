967bcc0528310e017d45fedc5b4185d23a8b72ab
Morena1233 <suedia.lima@hotmail.com>
2026-02-04 23:02:17 -0300

    Update sorteio.js

--- js/sorteio.js
+++ js/sorteio.js
@@ -1,1 +1,17 @@
-console.log("sorteio carregado");
+let numerosSorteados = [];
+
+function sortearNumero() {
+  if (numerosSorteados.length >= 75) {
+    alert("Todos os números já foram sorteados");
+    return;
+  }
+
+  let numero;
+  do {
+    numero = Math.floor(Math.random() * 75) + 1;
+  } while (numerosSorteados.includes(numero));
+
+  numerosSorteados.push(numero);
+  document.getElementById("numero").innerText = numero;
+}
+
