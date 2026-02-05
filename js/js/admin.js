6f00b84a0a087172b098e52514fcfe1c7741e365
Morena1233 <suedia.lima@hotmail.com>
2026-02-04 23:03:02 -0300

    Update admin.js

--- js/js/admin.js
+++ js/js/admin.js
@@ -1,1 +1,11 @@
-console.log("admin carregado");
+function salvarConfig() {
+  const valor = document.getElementById("valor").value;
+  const horario = document.getElementById("horario").value;
+
+  localStorage.setItem("valorCartela", valor);
+  localStorage.setItem("horarioSorteio", horario);
+
+  document.getElementById("status").innerText =
+    "Configuração salva com sucesso!";
+}
+
